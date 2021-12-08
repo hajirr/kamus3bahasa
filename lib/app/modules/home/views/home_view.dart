607bbbcefbd7284957e0785e7b2758b66f2a90c7
hajirr <xshitcode@gmail.com>
2021-12-08 13:24:28 +0700

    major update

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -11,8 +11,13 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
-          title: const Text('Kamus 3 Bahasa'),
+          title: Text(
+            'Kamus 3 Bahasa',
+            style: GoogleFonts.buda(
+                color: Colors.black, fontWeight: FontWeight.bold),
+          ),
           centerTitle: true,
           elevation: 0,
           backgroundColor: Colors.white,
@@ -20,24 +25,41 @@ class HomeView extends GetView<HomeController> {
         body: SizedBox(
           width: Get.width,
           height: Get.height,
-          child: GetBuilder<HomeController>(
-            builder: (_) {
-              return ListView.builder(
-                  itemCount: controller.results.length,
-                  itemBuilder: (context, index) {
-                    return controller.isLoading
-                        ? const CircularProgressIndicator()
-                        : ListTile(
-                            title: Text(controller.results[index].bahasa,
-                                style: GoogleFonts.roboto(color: Colors.black)),
-                            subtitle: Text(controller.results[index].bebasan,
-                                style: GoogleFonts.roboto()),
-                            leading: Text(
-                                controller.results[index].abjad.toUpperCase(),
-                                style: GoogleFonts.roboto()),
-                          );
-                  });
-            },
+          child: Column(
+            children: [
+              const Spacer(),
+              Divider(
+                thickness: 4,
+                color: Colors.grey[200],
+              ),
+              SizedBox(
+                height: Get.height / 1.5,
+                child: GetBuilder<HomeController>(
+                  builder: (_) {
+                    return ListView.builder(
+                        itemCount: controller.results.length,
+                        itemBuilder: (context, index) {
+                          return controller.isLoading
+                              ? const CircularProgressIndicator()
+                              : ListTile(
+                                  title: Text(controller.results[index].bahasa,
+                                      style: GoogleFonts.roboto(
+                                          color: Colors.black)),
+                                  subtitle: Text(
+                                      controller.results[index].bebasan,
+                                      style: GoogleFonts.roboto()),
+                                  leading: Text(
+                                      controller.results[index].abjad
+                                          .toUpperCase(),
+                                      style: GoogleFonts.roboto()),
+                                  trailing:
+                                      Text(controller.results[index].english),
+                                );
+                        });
+                  },
+                ),
+              ),
+            ],
           ),
         ));
   }
