059407b54b21a70a3a64dbaeb2bb208f77e7eade
mac <mac@macs-MacBook-Pro.local>
2021-12-09 20:24:25 +0700

    add detail page

--- lib/app/modules/detail/views/detail_view.dart
+++ lib/app/modules/detail/views/detail_view.dart
@@ -5,6 +5,8 @@ import 'package:get/get.dart';
 import '../controllers/detail_controller.dart';
 
 class DetailView extends GetView<DetailController> {
+  const DetailView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
